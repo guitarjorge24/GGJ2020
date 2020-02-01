fd1a44ebab95c8b115bbe295984ee686580b8f5c
Kristopher <kristopher.stuppel@ubalt.edu>
2020-02-01 14:52:06 -0500

    added Reset script and prefab

---
+++ Assets/Scripts/ResetScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetScript : MonoBehaviour
+{
+    private int sceneMemory, resetCount; //for later and tracking resets
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Reset"))
+        {
+            //begin resetting
+            //code to get scene where reset occured, just in case;
+            sceneMemory = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Debug.Log("should reset");
+        }
+    }
+}

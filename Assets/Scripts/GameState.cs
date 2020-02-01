c6560d4f30c4aeb7c59e40259986f8e47a9f3325
Daniel <damianscape@gmail.com>
2020-01-31 19:57:38 -0500

    Added some basic scene layout and initial files for traps and a game over condition

---
+++ Assets/Scripts/GameState.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour
+{
+    public float timer;
+
+    private void Update()
+    {
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            SceneManager.LoadScene("GameOver");
+        }
+    }
+}
---
+++ Assets/Scripts/PitfallTrap.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PitfallTrap : Trap
+{
+    private Vector2 startPosition;
+
+    void Start()
+    {
+        startPosition = GameObject.FindWithTag("StartPoint").transform.position;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            collision.gameObject.transform.position = startPosition;
+        }
+    }
+}

2c306461f5db77e01bc107bf8096c7bab6a37b51
Daniel <damianscape@gmail.com>
2020-01-31 21:58:14 -0500

    added snare trap

---
+++ Assets/Scripts/SnareTrap.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnareTrap : Trap
+{
+    public float snareTimer = 3;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            PlayerMove playerMove = collision.GetComponent<PlayerMove>();
+            StartCoroutine(SnareTimer(playerMove));
+
+            playerMove.enabled = false;
+        }
+    }
+
+    IEnumerator SnareTimer(PlayerMove playerMove)
+    {
+        playerMove.enabled = false;
+        yield return new WaitForSeconds(snareTimer);
+        playerMove.enabled = true;
+    }
+
+}

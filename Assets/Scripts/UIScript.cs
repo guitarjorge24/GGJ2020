9ff14332b21e2930eae719a116a1eec6f2821010
Kristopher <kristopher.stuppel@ubalt.edu>
2020-02-01 19:56:00 -0500

    Battery Icon changed, now works as a meter; when it goes below 25%, will turn red. Canvas updated with new changes.

--- Assets/Scripts/UIScript.cs
+++ Assets/Scripts/UIScript.cs
@@ -5,16 +5,24 @@ using UnityEngine.UI;
 
 public class UIScript : MonoBehaviour
 {
-    public string BatteryString;
+    public string batteryString;
     public Text timerText;
+    public Slider batteryBar;
+    public Image batteryIcon, batteryBarFill;
 
     // Update is called once per frame
     void Update()
     {
+        batteryBar.value = GameState.timer / 400;
         if(PauseScript.isGameActive == true)
         {
-            BatteryString = GameState.timer.ToString("F0");
-            timerText.text = BatteryString;
+            batteryString = GameState.timer.ToString("F0");
+            timerText.text = batteryString;
+            if(GameState.timer < 100)
+            {
+                batteryIcon.color = Color.red;
+                batteryBarFill.color = Color.red;
+            }
         }
     }
 }
